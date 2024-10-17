8855660b121814bd9f44208e361c6406bc33a402
Thakur Nishant Mogha <103104970+nishant0001000@users.noreply.github.com>
2024-10-17 16:37:19 +0530

    Add files via upload

---
+++ Desktop-animation.js
@@ -0,0 +1,27 @@
+gsap.to(".logos img",{
+    transform:"translatey(-2000%)",
+    duration:70,
+    repeat:-1,
+    ease:"none"
+})
+
+gsap.to(".mobilelogos img",{
+    transform:"translatey(-2000%)",
+    duration:70,
+    repeat:-1,
+    ease:"none"
+})
+
+gsap.to("#home-card",{
+    transform:"translatey(-2000%)",
+    duration:70,
+    repeat:-1,
+    ease:"none"
+})
+
+gsap.to("#mobilehome-card",{
+    transform:"translatey(-2000%)",
+    duration:70,
+    repeat:-1,
+    ease:"none"
+})
